9e2d14d352e17f83bed758780cbcda77b3a351f7
KirillZhel <49286886+KirillZhel@users.noreply.github.com>
2023-04-07 20:21:41 +0300

    added TR_Basket_insert_update script

---
+++ dbo.TR_Basket_insert_update.sql
@@ -0,0 +1,11 @@
+CREATE TRIGGER TR_Basket_insert_update
+ON [dbo].[Basket] 
+AFTER INSERT, UPDATE
+AS
+IF EXISTS(SELECT * FROM Inserted GROUP BY id_sku HAVING COUNT(*) >= 2)
+  UPDATE [dbo].[Basket] 
+    SET [dbo].[Basket].discountvalue = [dbo].[Basket].value * 0.05
+    WHERE [dbo].[Basket].id_sku IN (SELECT Inserted.id_sku FROM Inserted GROUP BY id_sku HAVING COUNT(*) >= 2)
+ELSE
+  UPDATE [dbo].[Basket] 
+    SET [dbo].[Basket].discountvalue = 0;
