b94eda4d9b972d404417583d1a43e4d729a2fae9
KirillZhel <49286886+KirillZhel@users.noreply.github.com>
2023-04-14 01:34:00 +0300

    Update dbo.usp_MakeFamilyPurchase.sql

--- dbo.usp_MakeFamilyPurchase.sql
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -1,15 +1,13 @@
 CREATE OR ALTER PROCEDURE usp_MakeFamilyPurchase (@FamilySurName varchar(255))
 AS
 BEGIN
-  UPDATE t1
-      SET t1.budgetvalue = t1.budgetvalue - t2.sumValue
-        FROM [dbo].[Family] AS t1
-          INNER JOIN (SELECT id_family, SUM(value) as sumValue FROM [dbo].[Basket] GROUP BY id_family) AS t2
-            ON t1.ID = t2.id_family
+	WITH sf AS (SELECT id_family, SUM(value) AS sumValue FROM [dbo].[Basket] GROUP BY id_family)
+  	UPDATE t1
+      SET t1.budgetvalue = t1.budgetvalue - sf.sumValue
+       	FROM [dbo].[Family] AS t1
+         	INNER JOIN sf ON t1.ID = sf.id_family
         WHERE t1.surname = @FamilySurName;
-        
-  DECLARE @count INT;
-    SELECT @count = Count(*) FROM [dbo].[Family] WHERE [dbo].[Family].surname = @FamilySurName;
-  IF @count = 0
+
+  IF(NOT EXISTS(SELECT 1 FROM [dbo].[Family] WHERE [dbo].[Family].surname = @FamilySurName))
       RAISERROR('Family %s does not exist in the database', 1, 1, @FamilySurName);
 END;
