69fa9e86dae3dd807225bc6da673e9af8f52387f
KirillZhel <49286886+KirillZhel@users.noreply.github.com>
2023-04-14 01:37:07 +0300

    Update dbo.TR_Basket_insert_update.sql

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -2,12 +2,9 @@ CREATE TRIGGER TR_Basket_insert_update
 ON [dbo].[Basket] 
 AFTER INSERT, UPDATE
 AS
-IF EXISTS(SELECT * FROM Inserted)
 BEGIN
-  UPDATE [dbo].[Basket] 
-    SET [dbo].[Basket].discountvalue = [dbo].[Basket].value * 0.05
-    WHERE [dbo].[Basket].id_sku IN (SELECT Inserted.id_sku FROM Inserted GROUP BY id_sku HAVING COUNT(*) >= 2);
-  UPDATE [dbo].[Basket] 
-    SET [dbo].[Basket].discountvalue = 0
-    WHERE [dbo].[Basket].id_sku IN (SELECT Inserted.id_sku FROM Inserted GROUP BY id_sku HAVING COUNT(*) = 1);
+  UPDATE [dbo].[Basket]
+    SET [dbo].[Basket]TR_Basket_insert_update.discountvalue = IIF(mi.c >= 2, [dbo].[Basket].value * 0.05, 0)
+    FROM (select inserted.id_sku as i, COUNT(inserted.id_sku) as c FROM inserted GROUP BY id_sku) as mi
+    WHERE mi.i = dbo.[Basket].[id_sku]
 END;
