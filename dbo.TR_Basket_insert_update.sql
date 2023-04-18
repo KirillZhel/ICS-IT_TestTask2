57bc89d9e24df8de072f58912030097c070bc748
KirillZhel <49286886+KirillZhel@users.noreply.github.com>
2023-04-18 23:24:11 +0300

    Update dbo.TR_Basket_insert_update.sql
    изменил оформление кода

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -3,8 +3,13 @@ ON [dbo].[Basket]
 AFTER INSERT, UPDATE
 AS
 BEGIN
-  UPDATE [dbo].[Basket]
-    SET [dbo].[Basket]TR_Basket_insert_update.discountvalue = IIF(mi.c >= 2, [dbo].[Basket].value * 0.05, 0)
-    FROM (select inserted.id_sku as i, COUNT(inserted.id_sku) as c FROM inserted GROUP BY id_sku) as mi
-    WHERE mi.i = dbo.[Basket].[id_sku]
+	UPDATE [dbo].[Basket]
+	SET [dbo].[Basket].discountvalue = IIF(mi.c >= 2, [dbo].[Basket].value * 0.05, 0)
+		FROM (
+			SELECT
+				inserted.id_sku AS i,
+				COUNT(inserted.id_sku) AS c 
+			FROM inserted 
+			GROUP BY id_sku) AS mi
+	WHERE mi.i = dbo.[Basket].[id_sku]
 END;
