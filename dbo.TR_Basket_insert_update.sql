de7414a96b64c8f00580557aff66bfc8843f8067
KirillZhel <49286886+KirillZhel@users.noreply.github.com>
2023-04-18 23:35:22 +0300

    Update dbo.TR_Basket_insert_update.sql

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -1,10 +1,10 @@
 CREATE TRIGGER TR_Basket_insert_update
-ON [dbo].[Basket] 
+ON dbo.[Basket] 
 AFTER INSERT, UPDATE
 AS
 BEGIN
-	UPDATE [dbo].[Basket]
-	SET [dbo].[Basket].discountvalue = IIF(mi.c >= 2, [dbo].[Basket].value * 0.05, 0)
+	UPDATE dbo.[Basket]
+	SET dbo.[Basket].discountvalue = IIF(mi.c >= 2, dbo.[Basket].value * 0.05, 0)
 		FROM (
 			SELECT
 				inserted.id_sku AS i,
